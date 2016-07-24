93992a1577642262099e959ae6e5e01c7c1cc555
Tom Houlé <tom@kafunsho.be>
2016-07-24 15:14:24 +0200

    Remove obsolete (?) test case
    It appears that calling `remove_object` with a non-existent object no
    longer throws an error.
    
    See https://github.com/minio/minio-py/issues/391

--- minio_storage/tests.py
+++ minio_storage/tests.py
@@ -76,9 +76,11 @@ class MinioStorageTests(TestCase):
         self.media_storage.delete(test_file)
         self.assertFalse(self.media_storage.exists(test_file))
 
-    def test_delete_of_non_existent_throws(self):
-        with self.assertRaises(IOError):
-            self.media_storage.delete("i don't even exist")
+    # Does not throw anymore
+    # See https://github.com/minio/minio-py/issues/391
+    # def test_delete_of_non_existent_throws(self):
+    #     with self.assertRaises(IOError):
+    #         self.media_storage.delete("i don't even exist")
 
     def test_url_generation(self):
         test_file = self.media_storage.save("weird & ÜRΛ",
