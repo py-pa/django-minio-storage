fd79655ff898b715273512cd8a655b262321444a
Thomas Frössman <thomasf@jossystem.se>
2017-06-04 13:58:50 +0200

    Fix minimum django version spec

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=['minio_storage'],
     setup_requires=['setuptools_scm'],
     install_requires=[
-        "django>=1.9",
+        "django>=1.8",
         "minio>=2.2.2",
     ],
     extras_require={
