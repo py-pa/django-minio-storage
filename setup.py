2e8ca37443316eeb9ea4d32f7bada8e29c4880dc
Thomas Frössman <thomasf@jossystem.se>
2017-09-19 19:12:21 +0200

    Update minio version to 2.2.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     setup_requires=['setuptools_scm'],
     install_requires=[
         "django>=1.8",
-        "minio>=2.2.3",
+        "minio>=2.2.4",
     ],
     extras_require={
         "test": [
