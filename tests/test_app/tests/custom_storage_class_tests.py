c43cb45773595761b7d5d1853aa1bc251140936d
Thomas Frössman <thomasf@jossystem.se>
2019-10-17 18:35:22 +0200

    add comments to example test

--- tests/test_app/tests/custom_storage_class_tests.py
+++ tests/test_app/tests/custom_storage_class_tests.py
@@ -32,7 +32,11 @@ class SecretStorage(MinioStorage):
 
     """
 
-    # We can set a new default File class implementation that will be used here
+    # We can set a new default File class implementation that will be used here because
+    # we want to stream the data directly from minio. Imagine that we need to process
+    # large files where we don't want to waste time/ram/disk space on  writing the file
+    # to disk two times before processing it.
+    #
     file_class = ReadOnlyMinioObjectFile
 
     def __init__(self, bucket_name=None):
@@ -40,15 +44,18 @@ class SecretStorage(MinioStorage):
         # we can create the minio client ourselves or use
         # create_minio_client_from_settings convinience function while providing it with
         # extra args.
+        #
         client = create_minio_client_from_settings(minio_kwargs={"region": "us-east-1"})
 
         # or use our own Django setting
+        #
         if bucket_name is None:
             bucket_name = get_setting("SECRET_BUCKET_NAME")
 
         # Run the super constructor and make a choice to only use presigned urls with
         # this bucket so that we can keep files more private here than how media files
         # usually are public readable.
+        #
         super().__init__(
             client,
             bucket_name,
@@ -61,23 +68,42 @@ class SecretStorage(MinioStorage):
 class CustomStorageTests(BaseTestMixin, TestCase):
     @override_settings(SECRET_BUCKET_NAME="my-secret-bucket")
     def test_custom_storage(self):
+
+        # Instansiate a storage class and put a file in it so that we have something to
+        # work with.
+        #
         storage = SecretStorage()
         storage_filename = storage.save("secret.txt", ContentFile(b"abcd"))
 
-        # create a temporary workspace directory
+        # Create a temporary workspace directory.
+        #
+        # It's importat that this directory is deleted after we are done so we use the
+        # with statement here.
+        #
         with tempfile.TemporaryDirectory() as workspace:
 
-            # a filename to use for the file inside the working directory.
+            # A filename to use for the file inside the working directory.
+            #
             filename = os.path.join(workspace, "secret.txt")
 
             # Open a stream with the minio file objenct and the temporary file.
+            #
+            # We might be processing a lot of files in a loop here so we are going top
+            # use the with statement to ensure that both the input stream and output
+            # files are closed after the copying is done.
+            #
             with open(filename, "wb") as out_file, storage.open(
                 storage_filename
             ) as storage_file:
 
-                # copy the stream from the http stream to the out_file
+                # Copy the stream from the http stream to the out_file
+                #
                 shutil.copyfileobj(storage_file.file, out_file)
 
+                #
+                # We are not using the ReadOnlyMinioObjectFile type so we can't seek in
+                # it.
+                #
                 with self.assertRaises(io.UnsupportedOperation):
                     storage_file.file.seek()
 
@@ -86,8 +112,9 @@ class CustomStorageTests(BaseTestMixin, TestCase):
 
             #
             # Process the file with external tools or something....
-            #  ...
             #
-
+            # For the purpouse of the example test we just check that the contents of
+            # the file is what we wrote in the beginning of the test.
+            #
             with open(filename, "rb") as f:
                 self.assertEqual(f.read(), b"abcd")
