py-pa/django-minio-storage

commit 86ff6ea4d64816b627223a72efd20e75aaea0a37

Update development tooling

.github/workflows/release.yml
@@ -14,9 +14,9 @@ jobs:
       id-token: write
     environment: release
     steps:
-    - uses: actions/checkout@v3
+    - uses: actions/checkout@v4
     - name: Set up Python
-      uses: actions/setup-python@v4
+      uses: actions/setup-python@v5
       with:
         python-version: "3.x"
     - name: Install pypa/build
@@ -35,4 +35,3 @@ jobs:
         .
     - name: Publish package
       uses: pypa/gh-action-pypi-publish@release/v1
-

.github/workflows/tox.yml
@@ -14,24 +14,24 @@ jobs:
         python-version: ['3.9', '3.10', '3.11', '3.12', '3.13']
 
     env:
-      AWS_ACCESS_KEY_ID: minioadmin
-      AWS_SECRET_ACCESS_KEY: minioadmin
       MINIO_STORAGE_ENDPOINT: 127.0.0.1:9000
       MINIO_STORAGE_ACCESS_KEY: minioadmin
       MINIO_STORAGE_SECRET_KEY: minioadmin
 
     services:
       minio:
-        image: fclairamb/minio-github-actions
+        # This image does not require any command arguments (which GitHub Actions don't support)
+        image: bitnami/minio:latest
+        env:
+          MINIO_ROOT_USER: minioadmin
+          MINIO_ROOT_PASSWORD: minioadmin
         ports:
           - 9000:9000
 
     steps:
-    - uses: actions/checkout@v3
-      with:
-        fetch-depth: 0
+    - uses: actions/checkout@v4
     - name: Set up Python ${{ matrix.python-version }}
-      uses: actions/setup-python@v4
+      uses: actions/setup-python@v5
       with:
         python-version: ${{ matrix.python-version }}
     - name: Install dependencies

.editorconfig
@@ -1,21 +1,19 @@
 root = true
 
 [*]
+indent_style = space
 end_of_line = lf
 insert_final_newline = true
+trim_trailing_whitespace = true
+charset = utf-8
 
 [*.py]
-charset = utf-8
-indent_style = space
 indent_size = 4
 max_line_length =  88
 
-[.travis.yml]
-indent_style = space
+[{*.yml,*.yaml}]
 indent_size = 2
 
 [*.json]
-charset = utf-8
-indent_style = space
 indent_size = 2
 max_line_length =  88

docker-compose.yml
@@ -1,10 +1,10 @@
-version: "2"
 services:
-    minio:
-        image: "minio/minio"
-        command: server /export
-        environment:
-            MINIO_ACCESS_KEY: weak_access_key
-            MINIO_SECRET_KEY: weak_secret_key
-        ports:
-            - "9153:9000"
+  minio:
+    image: minio/minio:latest
+    tty: true
+    command: ["server", "/export"]
+    environment:
+      MINIO_ROOT_USER: weak_access_key
+      MINIO_ROOT_PASSWORD: weak_secret_key
+    ports:
+      - 9153:9000

mkdocs.yml
@@ -1,8 +1,8 @@
 site_name: django-minio-storage
 theme: readthedocs
 nav:
-    - Home: index.md
-    - Usage: usage.md
-    - Development: development.md
-    - Contributing: contributing.md
-    - Licences: licences.md
+  - Home: index.md
+  - Usage: usage.md
+  - Development: development.md
+  - Contributing: contributing.md
+  - Licences: licences.md

pyproject.toml
@@ -66,3 +66,10 @@ select = [
     "W",
 ]
 ignore = ["E203"]
+
+[tool.pytest.ini_options]
+pythonpath = [".", "tests"]
+addopts = ["--tb=short"]
+python_files = ["tests.py", "test_*.py", "*_tests.py"]
+django_find_project = false
+DJANGO_SETTINGS_MODULE = "django_minio_storage_tests.settings"

tests/test_app/tests/upload_tests.py
@@ -82,7 +82,6 @@ class UploadTests(BaseTestMixin, TestCase):
         metadata_attrs = {
             "Accept-Ranges",
             "Content-Length",
-            "Content-Security-Policy",
             "Content-Type",
             "ETag",
             "Last-Modified",
@@ -93,7 +92,7 @@ class UploadTests(BaseTestMixin, TestCase):
             "Date",
         }
         assert res.metadata is not None
-        self.assertTrue(metadata_attrs.issubset(res.metadata.keys()))
+        assert metadata_attrs <= set(res.metadata.keys())
 
 
 @override_settings(

tox.ini
@@ -1,10 +1,10 @@
 [tox]
-envlist =
+env_list =
     # All Pythons, oldest Django
     {py39,py310,py311,py312,py313}-django42-minioknown
     # Newest Python, all Djangos
     py313-django{42,51,52}-minioknown
-    # Newest PYthon, newest Django, newest Minio
+    # Newest Python, newest Django, newest Minio
     py313-django52-minio
     lint
     docs
@@ -18,21 +18,14 @@ python =
     3.12: py312
     3.13: py313, lint, docs, pyright
 
-[pytest]
-pythonpath = . tests
-DJANGO_SETTINGS_MODULE = django_minio_storage_tests.settings
-addopts=--tb=short
-python_files = tests.py test_*.py *_tests.py
-django_find_project = false
-
 [testenv]
-commands = pytest {posargs}
-setenv =
-    PYTHONDONTWRITEBYTECODE=1
-    MINIO_STORAGE_ENDPOINT={env:MINIO_STORAGE_ENDPOINT:localhost:9153}
-    MINIO_STORAGE_ACCESS_KEY={env:MINIO_STORAGE_ACCESS_KEY:weak_access_key}
-    MINIO_STORAGE_SECRET_KEY={env:MINIO_STORAGE_SECRET_KEY:weak_secret_key}
-    TOX_ENVNAME={envname}
+package = wheel
+set_env =
+    PYTHONDONTWRITEBYTECODE = 1
+    MINIO_STORAGE_ENDPOINT = {env:MINIO_STORAGE_ENDPOINT:localhost:9153}
+    MINIO_STORAGE_ACCESS_KEY = {env:MINIO_STORAGE_ACCESS_KEY:weak_access_key}
+    MINIO_STORAGE_SECRET_KEY = {env:MINIO_STORAGE_SECRET_KEY:weak_secret_key}
+    TOX_ENVNAME = {envname}
 deps =
     django42: Django==4.2.*
     django51: Django==5.1.*
@@ -40,32 +33,37 @@ deps =
     minio: minio
     minioknown: minio==7.1.12
     -rdev-requirements.txt
+commands =
+    pytest {posargs}
 
 [testenv:py313-django52-minioknown]
-commands = pytest --cov --cov-append --cov-report=term-missing {posargs}
+commands =
+    pytest --cov --cov-append --cov-report=term-missing {posargs}
 
 [testenv:coverage-report]
-deps = coverage[toml]
-skip_install = true
+package = skip
+depends = py313-django52-minioknown
+deps =
+    coverage[toml]
 commands =
     coverage report
     coverage html
-depends=py313-django52-minioknown
 
 [testenv:pyright]
+package = editable
+depends = py313-django52-minio
 deps =
     pyright
-    minio
-    django-stubs==4.2.*
-    Django==4.2.*
+    django-stubs==5.2.*
     types-requests
     -rdev-requirements.txt
 commands =
     pyright --level WARNING
 
 [testenv:lint]
-setenv=
-    PYTHONWARNINGS=ignore
+package = skip
+set_env=
+    PYTHONWARNINGS = ignore
 deps =
     ruff==0.11.8
 commands =
@@ -73,8 +71,9 @@ commands =
     ruff format --check
 
 [testenv:fmt]
-setenv=
-    PYTHONWARNINGS=ignore
+package = skip
+set_env=
+    PYTHONWARNINGS = ignore
 deps =
     ruff==0.11.8
 commands =
@@ -82,5 +81,8 @@ commands =
     ruff format
 
 [testenv:docs]
-deps = mkdocs
-commands = mkdocs build
+package = skip
+deps =
+    mkdocs
+commands =
+    mkdocs build

commit 5daf047a4148fef01862caf5b7c4b79ae3e28539

Ensure the package version can always be determined in CI
This fixes a small bug introduced by #151, where version numbers weren't
computed correctly during CI tests.

This also fixes a larger pre-existing bug, where version numbers might
not always be computed correctly during CI releases.

Also add a test to prevent future regressions.

.github/workflows/release.yml
@@ -15,6 +15,8 @@ jobs:
     environment: release
     steps:
     - uses: actions/checkout@v4
+      with:
+        fetch-depth: 0
     - name: Set up Python
       uses: actions/setup-python@v5
       with:

.github/workflows/tox.yml
@@ -30,6 +30,8 @@ jobs:
 
     steps:
     - uses: actions/checkout@v4
+      with:
+        fetch-depth: 0
     - name: Set up Python ${{ matrix.python-version }}
       uses: actions/setup-python@v5
       with:

tests/test_app/tests/package_tests.py
@@ -1,0 +1,18 @@
+from importlib.metadata import version as package_version
+
+from packaging.version import parse as parse_version
+
+# Any version less than this is a bug, but versions greater are fine
+MIN_VERSION = parse_version("0.5.7")
+
+
+def test_package_version_file():
+    from minio_storage.version import __version__
+
+    version = parse_version(__version__)
+    assert version >= MIN_VERSION
+
+
+def test_package_version_metadata():
+    version = parse_version(package_version("django_minio_storage"))
+    assert version >= MIN_VERSION